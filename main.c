a090cd1c1ca635a1d510b82454a69ef9960648cc
general_debian <general_debian>
2018-09-03 09:56:45 +0100

    Initial load of ht eHelloWorld project.

---
+++ main.c
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: general
+ *
+ * Created on 03 September 2018, 09:48
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    printf("Hello Show and Tell Team and Github.\n");
+    return (EXIT_SUCCESS);
+}
+
